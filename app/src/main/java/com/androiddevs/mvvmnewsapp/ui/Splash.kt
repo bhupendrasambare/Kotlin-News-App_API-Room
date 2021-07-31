63a81e852207066b69bb0467923cfa3d7eb0d942
Bhupendra sambare <bhupendrasam1404@Bhupendras-MacBook-Air.local>
2021-07-31 08:44:08 +0530

    Project Build

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/Splash.kt
@@ -0,0 +1,19 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.androiddevs.mvvmnewsapp.R
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val intent = Intent(this, NewsActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "c9b7980904f1433aa33e214824a77472"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
